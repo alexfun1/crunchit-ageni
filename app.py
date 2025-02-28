fa4f0bb845136a4712a4395a7184fe35ba3728a6
deployster <infra-ops@wgames.io>
2025-02-28 09:56:49 -0500

    initial iface

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import gradio as gr
 import requests
+import json
+import os
+
+def load_config():
+    config_path = os.path.join(os.path.dirname(__file__), 'config.json')
+    with open(config_path, 'r') as config_file:
+        config = json.load(config_file)
+    return config
+
 
 
 def automatic_get_output(prompt, neg_prompt):
@@ -16,8 +25,8 @@ def automatic_get_output(prompt, neg_prompt):
         return images
     else:
         response.raise_for_status()
-        
-def gen_automatic():
+
+def gen_image():
     automatic = gr.Blocks()
     with automatic:
         with gr.Row():
@@ -30,21 +39,33 @@ def gen_automatic():
             
     return automatic
 
-def gen_civitai():
+def get_user_personal_data(id):
+    users_path = os.path.join(os.path.dirname(__file__), 'users.json')
+    with open(users_path, 'r') as users_file:
+        users = json.load(users_file)
+    user_data = users.get(str(id), {})
+    return user_data
+
+
+def gen_user_mgmt_tab():
     civitai = gr.Blocks()
     with civitai:
         with gr.Row():
             with gr.Column():
-                prompt = gr.Textbox()
-                neg_prompt = gr.Textbox()
-                civitai_submit = gr.Button()
+                login = gr.Textbox()
+                password = gr.Textbox()
+                mana = gr.Textbox()
+                buy_mana = gr.Button()
             with gr.Column():
                 civitai_output = gr.Gallery()
     return civitai
 
 def gen_canva():
-    t_interface = gr.TabbedInterface([gen_automatic(), gen_civitai()], ["Automatic", "Civitai"])
+    t_interface = gr.TabbedInterface([gen_automatic(), gen_user_mgmt_tab()], ["Automatic", "Civitai"])
     return t_interface
+
+
+config = load_config()
 
 iface = gen_canva()
 iface.launch()
